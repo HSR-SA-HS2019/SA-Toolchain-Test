e581c7c5229b6c730d226538fa8366b24c99960c
martinhug <mhug@hsr.ch>
2019-09-25 15:05:31 +0200

    add vis.js und Testgraph

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Graph from "react-graph-vis"
 
-ReactDOM.render(<App/>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+var graph = {
+    nodes: [
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+    ],
+    edges: [
+        {from: 1, to: 2},
+        {from: 1, to: 3},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+    ]
+};
+
+var options = {
+    layout: {
+        hierarchical: true
+    },
+    edges: {
+        color: "#000000"
+    }
+};
+
+var events = {
+    select: function(event) {
+        var { nodes, edges } = event;
+    }
+}
+
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Graph graph={graph} options={options} events={events} style = {{height: "640px" }}/>, rootElement);
+// ReactDOM.render(<Game />, rootElement);m
+//ReactDOM.render(<App />, rootElement);
